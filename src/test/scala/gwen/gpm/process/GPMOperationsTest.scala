99fecfd95cd0879bf048dce63d204571b07d0aaa
Branko Juric <branko.juric@gmail.com>
2017-08-22 00:03:08 +1000

    delete lock file in each unit test

--- src/test/scala/gwen/gpm/process/GPMOperationsTest.scala
+++ src/test/scala/gwen/gpm/process/GPMOperationsTest.scala
@@ -26,9 +26,11 @@ import org.scalatest.{FlatSpec, Matchers}
 class GPMOperationsTest extends FlatSpec with Matchers {
 
   private val rootDir = new File(s"${FileIO.userHomeDir.getPath}/.gwen")
+  private val lockFile = new File(rootDir, ".lock")
   private val settings = new GPMSettings(Nil)
 
   "gwen-web package" should "install" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"${rootDir.getPath}/package/gwen-web")
     val options = GPMOptions(Operation.install, GPackage.gwen_web, "latest", None)
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -38,6 +40,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "gwen-web package" should "install into external dir" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"target/gwen-package/gwen-web")
     val options = GPMOptions(Operation.install, GPackage.gwen_web, "latest", Some(destDir))
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -47,6 +50,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "gwen-web package" should "update" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"${rootDir.getPath}/package/gwen-web")
     val options = GPMOptions(Operation.update, GPackage.gwen_web, "latest", None)
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -56,6 +60,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "gwen-web package" should "update into external dir" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"target/gwen-package/gwen-web")
     val options = GPMOptions(Operation.update, GPackage.gwen_web, "latest", Some(destDir))
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -65,6 +70,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "chrome-driver package" should "install" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"${rootDir.getPath}/package/chrome-driver")
     val options = GPMOptions(Operation.install, GPackage.chrome_driver, "latest", None)
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -74,6 +80,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "chrome-driver package" should "install in external dir" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"target/gwen-package/chrome-driver")
     val options = GPMOptions(Operation.install, GPackage.chrome_driver, "latest", Some(destDir))
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -83,6 +90,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "chrome-driver package" should "update" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"${rootDir.getPath}/package/chrome-driver")
     val options = GPMOptions(Operation.update, GPackage.chrome_driver, "latest", None)
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -92,6 +100,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "chrome-driver package" should "update in external dir" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"target/gwen-package/chrome-driver")
     val options = GPMOptions(Operation.update, GPackage.chrome_driver, "latest", Some(destDir))
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -101,6 +110,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "gecko-driver package" should "install" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"${rootDir.getPath}/package/gecko-driver")
     val options = GPMOptions(Operation.install, GPackage.gecko_driver, "latest", None)
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -110,6 +120,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "gecko-driver package" should "install in external dir" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"target/gwen-package/gecko-driver")
     val options = GPMOptions(Operation.install, GPackage.gecko_driver, "latest", Some(destDir))
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -119,6 +130,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "gecko-driver package" should "update" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"${rootDir.getPath}/package/gecko-driver")
     val options = GPMOptions(Operation.update, GPackage.gecko_driver, "latest", None)
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -128,6 +140,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "gecko-driver package" should "update in external dir" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"target/gwen-package/gecko-driver")
     val options = GPMOptions(Operation.update, GPackage.gecko_driver, "latest", Some(destDir))
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -137,6 +150,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "selenium package" should "install" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"${rootDir.getPath}/package/selenium")
     val options = GPMOptions(Operation.install, GPackage.selenium, "latest", None)
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -146,6 +160,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
    "selenium package" should "install in external dir" in {
+     if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"target/gwen-package/selenium")
     val options = GPMOptions(Operation.install, GPackage.selenium, "latest", Some(destDir))
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -155,6 +170,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "selenium package" should "update" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"${rootDir.getPath}/package/selenium")
     val options = GPMOptions(Operation.update, GPackage.selenium, "latest", None)
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
@@ -164,6 +180,7 @@ class GPMOperationsTest extends FlatSpec with Matchers {
   }
 
   "selenium package" should "update in external dir" in {
+    if (lockFile.exists()) lockFile.delete()
     val destDir = new File(s"target/gwen-package/selenium")
     val options = GPMOptions(Operation.update, GPackage.selenium, "latest", Some(destDir))
     new GPMOperations(options, settings).install().getPath should be (destDir.getPath)
